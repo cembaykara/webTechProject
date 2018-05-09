e0cc194892f0a39f0f70aa22c33c91ad7dc94c6c
cembaykara <babayk@ttu.ee>
2018-05-09 21:50:39 +0300

    push jobpost handler

---
+++ mywebapp/application/postJob.php
@@ -0,0 +1,55 @@
+<?php
+
+session_start();
+
+require_once ("database/DatabaseConnection.php");
+/**
+ * This is the function that handles the registration
+ */
+function post() {
+
+    $userID = $_SESSION['userID'];
+    $postedData = $_POST['data'];
+
+    $title = $postedData['title'];
+    $body = $postedData['body'];
+
+
+    //TODO: we should validate our data before inserting to database
+
+    // create PDO connection object
+    $dbConn = new DatabaseConnection();
+    $pdo = $dbConn->getConnection();
+
+    // insert using PDO prepared statement, it helps prevents against sql injection attack (more on that later)
+    $params = [
+        ':title' => $title,
+        ':body' => $body,
+        ':fk_from_user' => $userID
+    ];
+
+    try {
+        $statement = $pdo->prepare(
+            "INSERT INTO `job` (`fk_from_user`, `title`, `body`) 
+                          VALUES (:fk_from_user, :title, :body)"
+        );
+
+        $statement->execute($params);
+
+        if ($pdo->lastInsertId()) {
+            echo "Posted";
+            return;
+        }
+
+    } catch (PDOException $e) {
+        // usually this error is logged in application log and we should return an error message that's meaninful to user
+        echo $e->getMessage();
+    }
+
+    echo "Posted";
+
+    return;
+}
+
+// call to the register function
+post();
---
+++ mywebapp/postjob.php
@@ -0,0 +1,40 @@
+<!-- ******************************************
+*  Author : Baris Cem Baykara   
+*  Created On : Wed May 09 2018
+*  File : postjob.html
+******************************************* -->
+<?php
+require_once ('protected_access_check.php');
+require_once ('application/models/User.php');
+
+/**
+ * @var $userData User
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Baris Cem Baykara">
+    <title>Create Job Listing</title>
+</head>
+
+<body>
+    <form id="postJob" action="application/postJob.php" method="POST">
+        <p>
+            <label>title: </label>
+            <input type="text" name="data[title]"/>
+        <p>
+        <p>
+            <label>body: </label>
+            <input type="text" name="data[body]"/>
+        <p>
+            <input type="reset" name="btnClear" value="Clear" class="button"/>
+            <input type="submit" name="btnSubmit" value="Post" class="button marL10"/>
+        <p>
+    </form>
+</body>
+
+</html>
