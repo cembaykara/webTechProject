744ca3ca5c8c6ca45f7e7cfd54bdef70310906e8
Merih Kaner <mekane@ttu.ee>
2018-05-11 00:34:35 +0300

    profile page updated

--- mywebapp/profile.php
+++ mywebapp/profile.php
@@ -6,109 +6,115 @@ require_once ('application/models/User.php');
  * @var $userData User
  */
 ?>
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>::Profile::</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <link rel="stylesheet" href="/mywebapp/css/main.css" type="text/css">
-    <link rel="shortcut icon" href="/mywebapp/images/favicon.ico?v=2" type="image/x-icon"/>
+	<title>ata$a | profile</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
 </head>
 <body>
-<div id="wrapper">
-    <div id="maincontent">
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
 
-        <div id="header">
-            <div id="logo" class="left">
-                <a href="/mywebapp">ICD0007</a>
-            </div>
-            <div class="right marT10">
-                <b>
-                    <a href="/mywebapp/logout.php">Logout</a>
-                </b>
-            </div>
-            <br><br>
-            <ul class="topmenu">
-                <li><a href="/mywebapp">Home</a></li>
-                <li><a href="/mywebapp">Student Lists</a></li>
-                <li><a href="/mywebapp">Contact Us</a></li>
-            </ul>
-            <br>
-            <div class="banner"><p></p></div>
-            <br class="clear"/>
-        </div>
+				<div class="content">
+					<?php
+                	if (isset($_SESSION['error_message'])) {
+                    	echo '<p>' . $_SESSION['error_message'] . '</p>';
+                	} elseif (isset($_SESSION['success_message'])) {
+                    	echo '<p>' . $_SESSION['success_message'] . '</p>';
+                	}
+                	?>
 
-        <div class="content">
-            <br/>
-            <div class="content-area">
-                <h2>Update profile</h2>
-                <br/>
-                <?php
-                if (isset($_SESSION['error_message'])) {
-                    echo '<p>' . $_SESSION['error_message'] . '</p>';
-                } elseif (isset($_SESSION['success_message'])) {
-                    echo '<p>' . $_SESSION['success_message'] . '</p>';
-                }
-                ?>
-                <form id="updateForm" action="application/updateHandler.php" method="POST" enctype="multipart/form-data">
-                    <input type="hidden" name="data[id]" value="<?= $userData['id']; ?>"/>
-                    <p>
-                        <label>Email: </label>
-                        <input type="text" name="data[email]" value="<?= $userData['email']; ?>"/>
-                    <p>
-                    <p>
-                        <label>First Name: </label>
-                        <input type="text" name="data[firstname]" value="<?= $userData['firstname']; ?>"/>
-                    <p>
-                    <p>
-                        <label>Last Name: </label>
-                        <input type="text" name="data[lastname]" value="<?= $userData['lastname']; ?>"/>
-                    <p>
-                    <p>
-                        <label>Address: </label>
-                        <textarea name="data[address]"><?= $userData['address']; ?></textarea>
-                    <p>
-                    <p>
-                        <label>City: </label>
-                        <input type="text" name="data[city]" value="<?= $userData['city']; ?>"/>
-                    <p>
-                    <p>
-                        <label>Postcode: </label>
-                        <input type="text" name="data[postcode]" value="<?= $userData['postal_code']; ?>"/>
-                    <p>
-                    <p>
-                        <label>Telephone: </label>
-                        <input type="text" name="data[telephone]" value="<?= $userData['telephone']; ?>"/>
-                    <p>
-                    <p>
-                        <label>Profile Picture: </label>
-                        <input type="file" name="fileToUpload" id="fileToUpload">
-                    <p>
-                    <p>
-                        <input type="submit" name="btnSubmit" value="Update profile" class="button marL10"/>
-                    <p>
-                </form>
-                <div id="profileImage">
-                    <img src="uploads/<?= $userData['profile_avatar'] ?>" width="300px" height="280px">
-                </div>
-            </div>
-        </div>
+				<form id="updateForm" class="login100-form validate-form" action="application/updateHandler.php" method="POST" enctype="multipart/form-data">
+					<span class="login100-form-title">
+						Profile
+					</span>
 
-    </div><!-- maincontent -->
-    <br>
-    <div id="footer">
-        <div class="footer">
-            Copyright &copy; 2018 ICD0007. <br/>
-            <a href="/mywebapp">Home</a> | <a href="about">About Us</a> | <a href="contact">Contact Us</a> <br/>
-            <span class="contact">Tel: +372-1111111&nbsp;
-			Email:icd007@icd0007.com</span>
-        </div>
-    </div><!-- footer -->
+					<input type="hidden" name="data[id]" value="<?= $userData['id']; ?>"/>
 
-</div><!-- wrapper -->
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="email" name="data[email]" value="<?= $userData['email']; ?>">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
 
+					<div class="wrap-input100 validate-input">
+						<input class="input100" type="text" name="data[firstname]" value="<?= $userData['firstname']; ?>">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-id-card" aria-hidden="false"></i>
+						</span>
+					</div>
+					
+					<div class="wrap-input100 validate-input">
+						<input class="input100" type="text" name="data[lastname]" value="<?= $userData['lastname']; ?>">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-id-card" aria-hidden="false"></i>
+						</span>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<button type="submit" class="login100-form-btn">
+							Update
+						</button>
+					</div>
+
+					<div class="text-center p-t-136">
+						<a class="txt2" href="logout.php">
+							logout
+							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	
+
+	
+<!--===============================================================================================-->	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+<?php
+unset($_SESSION['error_message']);
+unset($_SESSION['success_message']);
+?>
 </body>
 </html>
-
