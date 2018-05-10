fd4eb12c1f0169baa8a1c9d0aa9dfd55c8c75b47
cembaykara <babayk@ttu.ee>
2018-05-11 01:26:55 +0300

    no vendor

--- mywebapp/application/updateHandler.php
+++ mywebapp/application/updateHandler.php
@@ -16,7 +16,7 @@ function update() {
     $dbConn = new DatabaseConnection();
     $pdo = $dbConn->getConnection();
 
-    $uploadedImage = uploadImage();
+    // $uploadedImage = uploadImage();
 
     $params = [
         ':firstname' => $firstname,
@@ -25,15 +25,15 @@ function update() {
         ':id' => $postedData['id']
     ];
 
-    if ($uploadedImage === false) {
-        $params[':profile_avatar'] = null;
-    } else {
-        $params[':profile_avatar'] = $uploadedImage;
-    }
+    // if ($uploadedImage === false) {
+    //     $params[':profile_avatar'] = null;
+    // } else {
+    //     $params[':profile_avatar'] = $uploadedImage;
+    // }
 
     try {
         $statement = $pdo->prepare(
-            "UPDATE `users` SET firstname = :firstname, lastname = :lastname, email = :email,
+            "UPDATE `users` SET firstname = :firstname, lastname = :lastname, email = :email
                         WHERE id = :id"
         );
 
@@ -51,51 +51,51 @@ function update() {
 /**
  * @return string
  */
-function uploadImage()
-{
-    // no file selected
-    if(empty($_FILES["fileToUpload"]['name'])) {
-        return '';
-    }
+// function uploadImage()
+// {
+//     // no file selected
+//     if(empty($_FILES["fileToUpload"]['name'])) {
+//         return '';
+//     }
 
-    $target_dir = __DIR__ . "/../uploads/";
-    $target_file = basename($_FILES["fileToUpload"]["name"]);
+//     $target_dir = __DIR__ . "/../uploads/";
+//     $target_file = basename($_FILES["fileToUpload"]["name"]);
 
-    // Check if image file is a actual image or fake image
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check === false) {
-        $_SESSION['error_message'] = "Invalid image file.";
-        return false;
-    }
+//     // Check if image file is a actual image or fake image
+//     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+//     if($check === false) {
+//         $_SESSION['error_message'] = "Invalid image file.";
+//         return false;
+//     }
 
-    // Check file size
-    if ($_FILES["fileToUpload"]["size"] > 500000) {
-        $_SESSION['error_message'] = "Sorry, your file is too large.";
-        return false;
-    }
+//     // Check file size
+//     if ($_FILES["fileToUpload"]["size"] > 500000) {
+//         $_SESSION['error_message'] = "Sorry, your file is too large.";
+//         return false;
+//     }
 
-    $allowedFileType = [
-        "jpg",
-        "png",
-        "jpeg",
-        "gif",
-    ];
+//     $allowedFileType = [
+//         "jpg",
+//         "png",
+//         "jpeg",
+//         "gif",
+//     ];
 
-    $imageFileType = strtolower(pathinfo($target_dir . $target_file, PATHINFO_EXTENSION));
+//     $imageFileType = strtolower(pathinfo($target_dir . $target_file, PATHINFO_EXTENSION));
 
-    // Allow certain file formats
-    if(!in_array($imageFileType, $allowedFileType)) {
-        $_SESSION['error_message'] = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        return false;
-    }
+//     // Allow certain file formats
+//     if(!in_array($imageFileType, $allowedFileType)) {
+//         $_SESSION['error_message'] = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+//         return false;
+//     }
 
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . $target_file)) {
-        return $target_file;
-    } else {
-        $_SESSION['error_message'] = "Sorry, there was an error uploading your file.";
-        return false;
-    }
-}
+//     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . $target_file)) {
+//         return $target_file;
+//     } else {
+//         $_SESSION['error_message'] = "Sorry, there was an error uploading your file.";
+//         return false;
+//     }
+// }
 
 unset($_SESSION['success_message']);
 unset($_SESSION['error_message']);
